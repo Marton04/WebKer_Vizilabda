9af5dc0d8d97b8900da898f4caec6d2f0e7384c8
Palotas Marton <h375892@stud.u-szeged.hu>
2025-04-06 21:02:19 +0200

    Home style

--- vizilabda-project/src/app/pages/home/home.component.ts
+++ vizilabda-project/src/app/pages/home/home.component.ts
@@ -1,11 +1,25 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent{
+  isLoggedIn = false;
+  ngOnInit(): void {
+    this.checkLoginStatus();
+  }
 
+  checkLoginStatus(): void {
+    this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  }
 }
